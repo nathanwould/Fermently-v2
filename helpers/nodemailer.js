50b455f38eb21386a915860f7e50220775bf0b7b
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2021-06-04 17:33:23 -0400

    installed auth dependencies

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -1,15 +1,15 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+// const nodemailer = require('nodemailer');
+// require('dotenv').config();
 
-const sendEmail = async (email) => {
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-  let info = await transporter.sendMail(email);
-}
+// const sendEmail = async (email) => {
+//   var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: process.env.EMAIL,
+//       pass: process.env.EMAIL_PASS
+//     }
+//   });
+//   let info = await transporter.sendMail(email);
+// }
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
